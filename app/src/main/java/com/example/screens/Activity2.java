86fd528356e5ca679b78668b40ebb9d91cf7b314
ChristianFast <112157470+ChristianFast@users.noreply.github.com>
2023-04-26 21:17:09 +0200

    Lagt in extras för att skicka data med intent. number är statisk medan text hämtar input från Edittext i main. Även lagt bundle i activity2. Dock ingen widget som visar data ännu.

--- app/src/main/java/com/example/screens/Activity2.java
+++ app/src/main/java/com/example/screens/Activity2.java
@@ -10,5 +10,13 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String name = extras.getString("text");
+            int number = extras.getInt("number");
+            // Do something with the name and number
+        }
+
     }
 }
--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,11 +21,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button1=findViewById(R.id.buttonHome);
+
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                EditText EditTextMain = findViewById(R.id.EditTextMain);
+                String passText = EditTextMain.getText().toString();
                 Intent intentNewActivity = new Intent(MainActivity.this,Activity2.class);
+                intentNewActivity.putExtra("number", 10);
+                intentNewActivity.putExtra("text", passText);
                 startActivity(intentNewActivity);
+
             }
         });
     }
