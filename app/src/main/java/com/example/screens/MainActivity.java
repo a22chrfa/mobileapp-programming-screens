c76892b0cd387c5849830a20d1dd102d1381dfbc
ChristianFast <112157470+ChristianFast@users.noreply.github.com>
2023-04-26 20:31:08 +0200

    Knappen länkas nu till activity2. På activity2 finns nu även texten "Gött mos"

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1=findViewById(R.id.buttonHome);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentNewActivity = new Intent(MainActivity.this,Activity2.class);
+                startActivity(intentNewActivity);
+            }
+        });
     }
 }
